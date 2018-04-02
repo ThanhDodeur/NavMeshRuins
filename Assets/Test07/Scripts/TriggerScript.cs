4f2acb21e1e258c3c795fa56eac8998b4059c4af
ThanhDodeur <thanhdodeur@gmail.com>
2018-04-02 15:28:10 +0200

    Base

---
+++ Assets/Test07/Scripts/TriggerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+
+    public GameObject wall1;
+    public GameObject wall2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(wall1);
+        Destroy(wall2);
+
+        // peut etre audio plus tard
+        
+    }
+}
